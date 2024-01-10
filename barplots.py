b2d6b6d168c12902904903d7f44cd0646a0c87f4
JTBuunk <152444523+JTBuunk@users.noreply.github.com>
2024-01-10 17:12:00 +0100

    Update barplots.py

--- barplots.py
+++ barplots.py
@@ -2,87 +2,76 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# File names and custom dataset names
-file_names = ["C:\\Users\\Johnt\\Documents\\GIMA\\Internship\\Bing_tile_results1.xlsx",
-              "C:\\Users\\Johnt\\Documents\\GIMA\\Internship\\Google_tile_results1.xlsx",
-              "C:\\Users\\Johnt\\Documents\\GIMA\\Internship\\OSM_tile_results1.xlsx"]
-dataset_names = ['Bing', 'Google', 'OSM']
+
+# replace with own datasets
+file_names = ["path_to_file",
+              "path_to_file",
+              "path_to_file",
+              "path_to_file",
+              "path_to_file"
+]
+
+# change names if neccessary
+dataset_names = ['Bing', 'Google', 'Google_conf', 'OSM', 'OMF']
 file_to_dataset = dict(zip(file_names, dataset_names))
 
 
-
-def clean_numeric(column):
-    # Attempt to convert to numeric, coercing errors into NaN
-    return pd.to_numeric(column, errors='coerce')
-
-# Read the Excel files and add an identifier column to each
+# read the Excel files and add an identifier column to each
 dataframes = []
 for file_name in file_names:
     df = pd.read_excel(file_name)
-    df['Dataset'] = file_to_dataset[file_name]  # Adding a new column to identify the dataset with a custom name
+    df['Dataset'] = file_to_dataset[file_name]  # adding a new column to identify the dataset with a custom name
     dataframes.append(df)
 
-# Combine the datasets
+# combine the datasets
 combined_data = pd.concat(dataframes, ignore_index=True)
 
 
 def fnr_by_threshold(data, variable, thresholds):
-    data = data.copy()  # Avoid SettingWithCopyWarning
+    data = data.copy()  # avoid SettingWithCopyWarning
 
-    # Check if thresholds are provided for the variable
+    # check if thresholds are provided for the variable
     if variable not in thresholds or len(thresholds[variable]) < 2:
         print(f"Not enough thresholds provided for {variable}.")
         return None, None
 
     try:
-        # Bin data into groups based on thresholds
+        # bin data into groups based on thresholds
         labels = [f'Q{i+1}' for i in range(len(thresholds[variable])-1)]
         data['Group'] = pd.cut(data[variable], bins=thresholds[variable], labels=labels, include_lowest=True)
 
-        # Calculate FNR for each group
+        # calculate FNR for each group
+        # change the formula for other fairness metrics
         fnr_groups = data.groupby('Group').apply(
-            lambda x: x['tp'].sum() / (x['fp'].sum() + x['tp'].sum()) if (x['tp'].sum() + x['fp'].sum()) > 0 else 0
+            lambda x: x['fp'].sum() / (x['fp'].sum() + x['tp'].sum()) if (x['fp'].sum() + x['tp'].sum()) > 0 else 0
         )
 
-        # Calculate the equality of opportunity
+        # calculate the equality of opportunity
         max_fnr = fnr_groups.max()
         min_fnr = fnr_groups.min()
         equality_of_opportunity = min_fnr / max_fnr if min_fnr != 0 else float('inf')
 
         return fnr_groups.reset_index(name='False_Negative_Rate'), equality_of_opportunity
 
-        # # Calculate average of count_rel for each group
-        # avg_count_rel_groups = data.groupby('Group')['count_rel'].mean()
-        # return avg_count_rel_groups.reset_index(name='Average_Count_Rel')
-
     except Exception as e:
         print(f"An error occurred while processing {variable}: {e}")
         return None, None
 
 
-variables = ['poverty', 'Pop_dens', 'Vul_pop', 'RWI', 'urban']
+# change with names of sensitive variables if necessary
+variables = ['Pop_dens', 'RWI', 'Rural_scale', 'Bld_size']
 thresholds = {
-    'poverty': [1.27, 10.95, 20.52, 31.51, 89.55],
-    'Pop_dens': [5.428,177.606,313.122,604.707,175585.941],
-    'Vul_pop': [0,0.0126,0.0244,0.0376,0.2527],
-    'RWI': [-1.757,-0.545,-0.307,0.071,2.287],
-    'urban': [1,12,17,21,30],
+
+    'Pop_dens': ['insert treshhold values here'],
+    'Rural_scale': ['insert treshhold values here'],
+    'RWI': ['insert treshhold values here'],
+    'Bld_size': ['insert treshhold values here'],
+
     }
 
-# Plotting and printing equality of opportunity
+
+# plotting and printing equality of opportunity
 sns.set_theme(style="whitegrid")
-
-def detect_outliers(data, variable):
-    Q1 = data[variable].quantile(0.25)
-    Q3 = data[variable].quantile(0.75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-
-    # Detect outliers
-    outliers_bool = (data[variable] < lower_bound) | (data[variable] > upper_bound)
-    outliers = data.loc[outliers_bool]
-    return outliers
 
 for variable in variables:
     plt.figure(figsize=(10, 6))
@@ -97,19 +86,37 @@ for variable in variables:
             plot_data = pd.concat([plot_data, quantile_data])
             print(f"{dataset_label}: {equality_of_opportunity:.2f}")
 
-            # # Print outliers for each quantile
-            # outliers = detect_outliers(quantile_data, 'False_Negative_Rate')
-            # if not outliers.empty:
-            #     print(f"Outliers for {variable} in {dataset_label} dataset:")
-            #     print(outliers)
 
-    # Plot the data without considering outliers
-    sns.barplot(data=plot_data, x='Group', y='False_Negative_Rate', hue='Dataset')
-    plt.title(f'{variable}')
-    plt.ylabel('FNR')
-    plt.xlabel('')
+    # plot the data
+    ax = sns.barplot(data=plot_data, x='Group', y='False_Negative_Rate', hue='Dataset')
+    ax.grid(False, axis='y')
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_fontweight('bold')
+
+    # set title and labels with bold font
+    ax.set_title(f'{variable}', fontweight='bold', color='black')
+    ax.set_ylabel('FNR', fontweight='bold', color='black')
+    ax.set_xlabel('', fontweight='bold', color='black')
+
+    # make left and bottom spines bold
+    ax.spines['left'].set_linewidth(2)
+    ax.spines['left'].set_color('black')
+    ax.spines['bottom'].set_linewidth(2)
+    ax.spines['bottom'].set_color('black')
+
+    # remove top and right spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+
 
     # Manually define the legend
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles=handles, labels=labels, title='Dataset', bbox_to_anchor=(1, 1), loc='upper left', title_fontsize='medium')
+
+    plt.subplots_adjust(right=0.35, top=0.5)
+    plt.show()
+
     handles, labels = plt.gca().get_legend_handles_labels()
     plt.legend(handles=handles, labels=labels, title='Dataset', bbox_to_anchor=(1, 1), loc='upper left')
     plt.show()
